2373a32254591386adc6f11710cff580f2218c4b
petii <mail@petii.dev>
2025-12-09 00:43:15 +0100

    day8: part1

--- aoc2025/src/day8.rs
+++ aoc2025/src/day8.rs
@@ -25,8 +25,7 @@ static EXAMPLE: &str = r#"
 425,690,689
 "#;
 
-#[derive(Debug, PartialOrd, Eq, Clone, PartialEq, Hash)]
-#[derive(Ord)]
+#[derive(Debug, PartialOrd, Eq, Clone, PartialEq, Hash, Ord)]
 struct Point3d(i128, i128, i128);
 impl Point3d {
     pub fn new(coords: Vec<i128>) -> Self {
@@ -58,7 +57,7 @@ fn parse_input(input: &str) -> Vec<Point3d> {
 fn part1(input: &str, connections: usize) -> u128 {
     let points = parse_input(input);
     let mut distances_for_pair = HashMap::new();
-    let mut points_for_distance = BTreeMap::<u128, Vec<(Point3d, Point3d)>>::new();
+    let mut points_for_distance: BTreeMap<u128, HashSet<_>> = BTreeMap::new();
     let mut distances = BTreeSet::new();
     for pair_v in combinations::Combinations::new(points.clone(), 2) {
         let [ref a, ref b] = pair_v[..] else { todo!() };
@@ -70,15 +69,16 @@ fn part1(input: &str, connections: usize) -> u128 {
         distances_for_pair.insert((b.clone(), a.clone()), distance);
 
         match points_for_distance.get_mut(&distance) {
-            Some(vector) => {
-                vector.push(pair);
+            Some(pairs) => {
+                pairs.insert(pair);
             }
             None => {
-                points_for_distance.insert(distance, vec![pair]);
+                let mut new_set = HashSet::new();
+                new_set.insert(pair);
+                points_for_distance.insert(distance, new_set);
             }
-        };
+        }
     }
-    dbg![distances_for_pair.get(&(Point3d(162, 817, 812), Point3d(425, 690, 689)))];
 
     let mut circuits = BTreeMap::<usize, HashSet<Point3d>>::new();
     let mut point_to_circuit = HashMap::new();
@@ -88,36 +88,32 @@ fn part1(input: &str, connections: usize) -> u128 {
         ps.insert(p);
         circuits.insert(i, ps);
     }
-    // dbg!(&point_to_circuit);
 
-    let smallest_dists = distances.iter().take(connections);
-    let empty = Vec::<(Point3d, Point3d)>::new();
-    let pairs_for_smallest_dists = smallest_dists
-        .fold(vec![], |acc, dist| {
-            dbg![dist, points_for_distance.get(dist)];
-            vec![
-                acc,
-                points_for_distance.get(dist).unwrap_or(&empty).to_vec(),
-            ]
-            .concat()
-        })
-        .into_iter()
-        .take(10);
+    // distances is ordered since is a BTreeSet
+    dbg![distances.len()];
+    let smallest_dists = distances.iter();
+    let pairs_for_smallest_dists =
+        smallest_dists.map(|dist| points_for_distance.get(dist).unwrap());
 
-    // dbg![pairs_for_smallest_dists.clone().collect::<Vec<_>>()];
-    for (a, b) in pairs_for_smallest_dists {
-        let a_circuit = point_to_circuit.get(&a).unwrap().clone();
-        let b_circuit = point_to_circuit.get(&b).unwrap().clone();
-        dbg![(a.clone(), a_circuit), (b.clone(), b_circuit)];
-        if a_circuit != b_circuit {
-            // merge!
-            let b_ps = circuits.get(&b_circuit).unwrap().clone();
-            let a_ps = circuits.get_mut(&a_circuit).unwrap();
-            for point in b_ps {
-                a_ps.insert(point.clone());
-                point_to_circuit.insert(point.clone(), a_circuit);
+    let mut made_connections = 0;
+    for pair_set in pairs_for_smallest_dists {
+        for (a, b) in pair_set {
+            let a_circuit = point_to_circuit.get(&a).unwrap().clone();
+            let b_circuit = point_to_circuit.get(&b).unwrap().clone();
+            if a_circuit != b_circuit {
+                // merge!
+                let b_ps = circuits.get(&b_circuit).unwrap().clone();
+                let a_ps = circuits.get_mut(&a_circuit).unwrap();
+                for point in b_ps {
+                    a_ps.insert(point.clone());
+                    point_to_circuit.insert(point.clone(), a_circuit);
+                }
+                circuits.remove(&b_circuit);
             }
-            circuits.remove(&b_circuit);
+            made_connections += 1;
+        }
+        if made_connections >= connections {
+            break;
         }
     }
     let mut circuit_sizes = Vec::new();
@@ -125,7 +121,14 @@ fn part1(input: &str, connections: usize) -> u128 {
         circuit_sizes.push(ps.len() as u128);
     }
     circuit_sizes.sort_by(|a, b| b.cmp(a));
-    dbg!(&circuit_sizes);
+    dbg!(
+        circuit_sizes.len(),
+        &circuit_sizes
+            .clone()
+            .into_iter()
+            .take(5)
+            .collect::<Vec<_>>()
+    );
     circuit_sizes
         .into_iter()
         .take(3)
