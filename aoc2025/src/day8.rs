579045714fa6ec7fed44463e2c760861c3f086a5
petii <mail@petii.dev>
2025-12-10 01:16:24 +0100

    day8: part2

--- aoc2025/src/day8.rs
+++ aoc2025/src/day8.rs
@@ -135,14 +135,84 @@ fn part1(input: &str, connections: usize) -> u128 {
         .fold(1, |acc, points| acc * points)
 }
 
+fn part2(input: &str) -> i128 {
+    let points = parse_input(input);
+    let mut distances_for_pair = HashMap::new();
+    let mut points_for_distance: BTreeMap<u128, HashSet<_>> = BTreeMap::new();
+    let mut distances = BTreeSet::new();
+    for pair_v in combinations::Combinations::new(points.clone(), 2) {
+        let [ref a, ref b] = pair_v[..] else { todo!() };
+        let pair = (a.clone(), b.clone());
+        let distance = a.distance(&b);
+        distances.insert(distance);
+
+        distances_for_pair.insert((a.clone(), b.clone()), distance);
+        distances_for_pair.insert((b.clone(), a.clone()), distance);
+
+        match points_for_distance.get_mut(&distance) {
+            Some(pairs) => {
+                pairs.insert(pair);
+            }
+            None => {
+                let mut new_set = HashSet::new();
+                new_set.insert(pair);
+                points_for_distance.insert(distance, new_set);
+            }
+        }
+    }
+
+    let mut circuits = BTreeMap::<usize, HashSet<Point3d>>::new();
+    let mut point_to_circuit = HashMap::new();
+    for (i, p) in points.into_iter().enumerate() {
+        point_to_circuit.insert(p.clone(), i);
+        let mut ps = HashSet::new();
+        ps.insert(p);
+        circuits.insert(i, ps);
+    }
+
+    // distances is ordered since is a BTreeSet
+    dbg![distances.len()];
+    let smallest_dists = distances.iter();
+    let pairs_for_smallest_dists =
+        smallest_dists.map(|dist| points_for_distance.get(dist).unwrap());
+
+    let mut latest_pair = (Point3d(0, 0, 0), Point3d(0, 0, 0));
+    for pair_set in pairs_for_smallest_dists {
+        for (a, b) in pair_set {
+            let a_circuit = point_to_circuit.get(&a).unwrap().clone();
+            let b_circuit = point_to_circuit.get(&b).unwrap().clone();
+            if a_circuit != b_circuit {
+                // merge!
+                let b_ps = circuits.get(&b_circuit).unwrap().clone();
+                let a_ps = circuits.get_mut(&a_circuit).unwrap();
+                for point in b_ps {
+                    a_ps.insert(point.clone());
+                    point_to_circuit.insert(point.clone(), a_circuit);
+                }
+                circuits.remove(&b_circuit);
+                if circuits.len() == 1 {
+                    latest_pair = (a.clone(), b.clone());
+                    break;
+                }
+            }
+        }
+        if circuits.len() == 1 {
+            break;
+        }
+    }
+    dbg![&latest_pair];
+
+    latest_pair.0.0 * latest_pair.1.0
+}
+
 fn main() {
     println!("example:\n{}", EXAMPLE);
     println!("part 1 = {}", part1(EXAMPLE, 10));
-    // println!("part 2 = {}", part2(EXAMPLE, true));
+    println!("part 2 = {}", part2(EXAMPLE));
     println!("input");
     let day8 = std::fs::read_to_string("inputs/day8.txt").unwrap();
     println!("part 1 = {}", part1(&day8, 1000));
-    // println!("part 2 = {}", part2(&day7, false));
+    println!("part 2 = {}", part2(&day8));
 }
 
 #[cfg(test)]
@@ -152,5 +222,10 @@ mod tests {
     #[test]
     fn example_part1() {
         assert_eq![part1(EXAMPLE, 10), 40];
+    }
+
+    #[test]
+    fn example_part2() {
+        assert_eq![part2(EXAMPLE), 25272];
     }
 }
